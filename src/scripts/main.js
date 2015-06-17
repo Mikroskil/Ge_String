d52e0580baf3ecac98cd845cfcc15c929396dc38
masdudut <masdudut001@gmail.com>
2015-06-17 17:27:26 +0700

    baru

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,8 @@ var bespoke = require('bespoke'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
-  backdrop = require('bespoke-backdrop'),
   scale = require('bespoke-scale'),
-  hash = require('bespoke-hash'),
-  progress = require('bespoke-progress'),
-  forms = require('bespoke-forms');
+  progress = require('bespoke-progress');
 
 // Bespoke.js
 bespoke.from('article', [
@@ -16,15 +13,6 @@ bespoke.from('article', [
   keys(),
   touch(),
   bullets('li, .bullet'),
-  backdrop(),
   scale(),
-  hash(),
-  progress(),
-  forms()
+  progress()
 ]);
-
-// Prism syntax highlighting
-// This is actually loaded from "bower_components" thanks to
-// debowerify: https://github.com/eugeneware/debowerify
-require('prism');
-
